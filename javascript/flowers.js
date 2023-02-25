799c7f84da7d8c7497ea4d2f04a1f762d0fd4862
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-02-25 23:14:12 +0800

    adding image

--- javascript/flowers.js
+++ javascript/flowers.js
@@ -1,15 +1,13 @@
 const addingImages = () => {
     for (var i = 0; i <= 22; i++) {
         var div = document.createElement("div");
-        div.setAttribute("class", "flower")
-        div.setAttribute("height", "400px");
-        div.setAttribute("width", "300px");
-
+        div.setAttribute("class", "flower flower" + i)
+        document.querySelector(".flowers-container").appendChild(div);
+        
         var image = document.createElement("img")
         image.setAttribute("src","../img/flower"+ i +".jpg");
-
-        document.querySelector(".flower").appendChild(image);
-        document.querySelector(".flowers-container").appendChild(div);
+        
+        document.querySelector(".flower" + i).appendChild(image);
    }
 }  //Image gallery
 
