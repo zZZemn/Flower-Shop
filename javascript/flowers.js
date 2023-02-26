62cc03c36d115a93fba44a3947e61c7f682ca35e
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-02-26 12:30:14 +0800

    flower name and price

--- javascript/flowers.js
+++ javascript/flowers.js
@@ -21,6 +21,9 @@ let flowerNames = [
     "Pretty in Pink Bouquet",
     "Pink Dazzler Bouquet"
   ];
+
+  let flowerPrices = [20.99, 15.49, 12.99, 18.75, 9.99, 24.99, 13.99, 16.50, 22.00, 14.99, 19.99, 11.99, 17.99, 27.50, 8.99, 21.00, 23.75, 10.99, 12.49, 18.00, 26.99, 7.99];
+
   
 
 const addingImages = () => {
@@ -32,12 +35,17 @@ const addingImages = () => {
         var image = document.createElement("img")
         image.setAttribute("src","../img/flower"+ i +".jpg");
 
-        var text = document.createElement("p")
-        text.setAttribute("class", "flower-name");
-        text.textContent = flowerNames[i];
+        var name = document.createElement("h5")
+        name.setAttribute("class", "flower-name");
+        name.textContent = flowerNames[i];
+
+        var price = document.createElement("p")
+        price.setAttribute("class", "price");
+        price.textContent = flowerPrices[i];
         
         document.querySelector(".flower" + i).appendChild(image);
-        document.querySelector(".flower" + i).appendChild(text);
+        document.querySelector(".flower" + i).appendChild(name);
+        document.querySelector(".flower" + i).appendChild(price);
    }
 }  //Image gallery
 
